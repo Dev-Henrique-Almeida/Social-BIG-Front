f0c92e3b73c9431456cd0329611aa37bbfec2103
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-07-16 14:42:54 -0300

    fix: data corrigida do perfil.

--- src/app/shared/utils/ConvertDates/convertDate.ts
+++ src/app/shared/utils/ConvertDates/convertDate.ts
@@ -1,6 +1,6 @@
 const FormatDateToBRFull = (dateString: string): string => {
   const date = new Date(dateString);
-  const day = date.getDate().toString().padStart(2, "0");
+  const day = (date.getDate() + 1).toString().padStart(2, "0");
   const monthNames = [
     "Janeiro",
     "Fevereiro",
