ef67d50e1a6b7e2faecd2b9f489ac22f35c7a9e3
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-07-17 18:28:40 -0300

    refactor: refatorando as pastas dos services.

--- src/app/shared/services/tests/posts/getAllPostsByUser.test.ts
+++ src/app/shared/services/tests/posts/getAllPostsByUser.test.ts
@@ -1,7 +1,7 @@
 import MockAdapter from "axios-mock-adapter";
-import { api } from "../api/api";
-import { getAllPostsByUser } from "../api/postApi";
-import { IPostData } from "../../@types";
+import { api } from "../../api/api";
+import { getAllPostsByUser } from "../../api/postApi";
+import { IPostData } from "../../../@types";
 
 describe("Testando a função getAllPostsByUser", () => {
   let mock: MockAdapter;
