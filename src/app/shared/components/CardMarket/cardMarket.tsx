e2980bf4e42d9333995529c7e8ebec4b715359d5
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-05 10:38:39 -0300

    feat: tela de listagem dos produtos no marketplace.

--- src/app/shared/components/CardMarket/cardMarket.tsx
+++ src/app/shared/components/CardMarket/cardMarket.tsx
@@ -1,20 +1,50 @@
 import React from "react";
 import { IMarketData } from "../../@types";
+import styles from "./cardMarket.module.scss";
+import useThemeStyles from "../../hooks/ThemeStyles/useThemeStyles";
 
 interface IMarketContent {
   market: IMarketData[];
 }
 
 const CardMarket: React.FC<IMarketContent> = ({ market }) => {
+  const themeStyles = useThemeStyles();
+
+  const truncateDescription = (description: string) => {
+    if (description.length > 120) {
+      return description.substring(0, 120) + "...";
+    }
+    return description;
+  };
+
   return (
-    <div>
+    <div className={styles.cardContainer}>
       {market.map((card) => (
-        <div key={card.id}>
-          <img src={card.image || "TESTE IMAGEM"} alt={card.name} />
-          <h1>{card.name}</h1>
-          <p>{card.description}</p>
-          <p>{card.price}</p>
-          <p>{card.vendido ? "TRUE" : "FALSE"} </p>
+        <div
+          key={card.id}
+          className={styles.card}
+          style={{
+            background: themeStyles.backgroundPaper,
+            color: themeStyles.textColor,
+          }}
+        >
+          <img
+            src={card.image || themeStyles.cardImage.src}
+            alt={card.name}
+            className={styles.cardImage}
+          />
+          <h1 className={styles.cardTitle}>{card.name}</h1>
+          <p className={styles.cardDescription}>
+            {truncateDescription(card.description)}
+          </p>
+          <p className={styles.cardPrice}>R$ {card.price}</p>
+          <p
+            className={`${styles.cardStatus} ${
+              card.vendido ? styles.sold : styles.available
+            }`}
+          >
+            {card.vendido ? "Já vendido!" : "Ainda não vendido."}
+          </p>
         </div>
       ))}
     </div>
--- src/app/shared/hooks/ThemeStyles/useThemeStyles.ts
+++ src/app/shared/hooks/ThemeStyles/useThemeStyles.ts
@@ -1,4 +1,6 @@
 import { useTheme } from "@mui/material";
+import cardImageDark from "../../../assets/card-padrao.png";
+import cardImageLight from "../../../assets/card-padrao-light.png";
 
 const useThemeStyles = () => {
   const theme = useTheme();
@@ -9,6 +11,7 @@ const useThemeStyles = () => {
     textColor: theme.palette.mode === "dark" ? "white" : "black",
     borderColor:
       theme.palette.mode === "dark" ? "1px solid #393a3e" : "1px solid #e0e0e0",
+    cardImage: theme.palette.mode === "dark" ? cardImageDark : cardImageLight,
   };
 };
 
